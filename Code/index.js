df5fe62fa395bbf8cfb96ea797c70498db288265
Ky_Dev25 <kylian.barthelemy2003@gmail.com>
2022-11-22 15:22:22 +0100

    ajout des commentaires

--- Code/index.js
+++ Code/index.js
@@ -2,6 +2,7 @@ const header = document.getElementById("header");
 const contentJoke = document.getElementById("content");
 const app = document.querySelector(".app");
 
+//Function to get API joke
 const getJoke = () => {
   fetch("https://api.blablagues.net/?rub=blagues")
     .then((res) => res.json())
@@ -15,6 +16,7 @@ const getJoke = () => {
 
 getJoke();
 
+//When app clicked, play function getJoke
 app.addEventListener("click", () => {
   getJoke();
 });
